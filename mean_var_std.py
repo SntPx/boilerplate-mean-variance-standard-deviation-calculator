6e88bd0ce07d15c9e6e6b3cab97700ee713d78ed
Christophe RIVIERE <meltdownfr@gmail.com>
2025-02-03 08:27:20 +0000

    Finished challenge

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,12 @@
 import numpy as np
 
-def calculate(list):
-
-
-
+def calculate(l):
+    if len(l) < 9:
+        raise ValueError("List must contain nine numbers.")
+    matrix = np.array(l).reshape(3,3)
+    operations = {"mean": np.mean, "variance": np.var, "standard deviation": np.std, "max": np.max, "min": np.min, "sum": np.sum}
+    calculations = {}
+    for k, v in operations.items():
+        calculations[k] = [v(matrix, 0).tolist(), v(matrix,1).tolist(), v(matrix)]
 
     return calculations
